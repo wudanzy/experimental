89f6a75e1284702bd01c288dc8b2611c308ae2a8
wudanzy <wudanzy@163.com>
2025-03-23 21:58:13 +0800

    Add another two APIs to the server.

--- nodejs/demo/app.js
+++ nodejs/demo/app.js
@@ -3,7 +3,18 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  console.log('Hello World from console!')
   res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+  console.log('/about')
+  res.send('This is a hello world demo for express js')
+})
+
+app.get('/users/:name', (req, res) => {
+  console.log(req.params)
+  res.send('Hello ' + req.params['name'])
 })
 
 app.listen(port, () => {
